b1ec230fdb8d267d69ecbcef76eb8f772fa63a59
David Rawson <dm.rawson@gmail.com>
2021-05-11 17:26:58 +0100

    Added react, redux and reducer files

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
