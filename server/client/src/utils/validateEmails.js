c562778e988f31d697ba238fb8ada3f52cc8a957
David Rawson <dm.rawson@gmail.com>
2021-06-02 22:16:44 +0100

    Survey review page displaying but not done the submit.

--- server/client/src/utils/validateEmails.js
+++ server/client/src/utils/validateEmails.js
@@ -1,3 +1,14 @@
+// eslint-disable-next-line
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// eslint-disable-next-line
 export default (emails) => {
-  const emailsArray = emails.split(",").map((email) => email.trim());
+  const invalidEmailsArray = emails
+    .split(",")
+    .map((email) => email.trim())
+    .filter((email) => re.test(email) === false);
+
+  if (invalidEmailsArray.length) {
+    return `These emails are invalid: ${invalidEmailsArray}`;
+  }
 };
